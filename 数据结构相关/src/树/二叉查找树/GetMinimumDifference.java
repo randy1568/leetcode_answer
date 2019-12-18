428fb5739f016bdd5df809c2834c14f0397390a2
randy156816 <836426879@qq.com>
2019-12-18 21:19:25 +0800

    530. 二叉搜索树的最小绝对差

--- 数据结构相关/src/树/二叉查找树/GetMinimumDifference.java
+++ 数据结构相关/src/树/二叉查找树/GetMinimumDifference.java
@@ -6,6 +6,30 @@ import 树.domain.TreeNode;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ 给定一个所有节点为非负值的二叉搜索树，求树中任意两节点的差的绝对值的最小值。
+
+ 示例 :
+
+ 输入:
+
+ 1
+ \
+ 3
+ /
+ 2
+
+ 输出:
+ 1
+
+ 解释:
+ 最小绝对差为1，其中 2 和 1 的差的绝对值为 1（或者 2 和 3）。
+ */
+
+/**
+ * 思路：
+ * 中序遍历得到一个升序集，再遍历升序集，得到最小差值
+ */
 public class GetMinimumDifference {
 
     private int min = Integer.MAX_VALUE;
